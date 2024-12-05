a3cf1ed32840b98b907eeff2172baa9f115c1d0d
Laurent Sorber <laurent.sorber@gmail.com>
2024-12-05 17:01:46 +0100

    test: parametrize test_extract

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -21,27 +21,28 @@ def llm(request: pytest.FixtureRequest) -> str:
     return llm
 
 
-def test_extract(llm: str) -> None:
+@pytest.mark.parametrize(
+    "strict",
+    [pytest.param(False, id="strict=False"), pytest.param(True, id="strict=True")],
+)
+def test_extract(llm: str, strict: bool) -> None:  # noqa: FBT001
     """Test extracting structured data."""
     # Set the LLM.
     config = RAGLiteConfig(llm=llm)
 
     # Define the JSON schema of the response.
     class LoginResponse(BaseModel):
-        model_config = ConfigDict(
-            extra="forbid"  # Forbid extra attributes as required by OpenAI's strict mode.
-        )
+        model_config = ConfigDict(extra="forbid" if strict else "allow")
         username: str = Field(..., description="The username.")
         password: str = Field(..., description="The password.")
         system_prompt: ClassVar[str] = "Extract the username and password from the input."
 
-    for strict in (False, True):
-        # Extract structured data.
-        username, password = "cypher", "steak"
-        login_response = extract_with_llm(
-            LoginResponse, f"{username} // {password}", strict=strict, config=config
-        )
-        # Validate the response.
-        assert isinstance(login_response, LoginResponse)
-        assert login_response.username == username
-        assert login_response.password == password
+    # Extract structured data.
+    username, password = "cypher", "steak"
+    login_response = extract_with_llm(
+        LoginResponse, f"{username} // {password}", strict=strict, config=config
+    )
+    # Validate the response.
+    assert isinstance(login_response, LoginResponse)
+    assert login_response.username == username
+    assert login_response.password == password
