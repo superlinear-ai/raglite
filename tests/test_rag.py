8c9dda8338ce91dd4b3d5b15102891303e394b9c
Laurent Sorber <laurent.sorber@gmail.com>
2024-12-25 23:23:45 +0100

    feat: apply llama-cpp-python streaming tool use

--- tests/test_rag.py
+++ tests/test_rag.py
@@ -1,10 +1,13 @@
 """Test RAGLite's RAG functionality."""
 
+import json
+
 from raglite import (
     RAGLiteConfig,
     create_rag_instruction,
     retrieve_rag_context,
 )
+from raglite._database import ChunkSpan
 from raglite._rag import rag
 
 
@@ -22,3 +25,38 @@ def test_rag_manual(raglite_test_config: RAGLiteConfig) -> None:
     assert "event" in answer.lower()
     # Verify that no RAG context was retrieved through tool use.
     assert [message["role"] for message in messages] == ["user", "assistant"]
+
+
+def test_rag_auto_with_retrieval(raglite_test_config: RAGLiteConfig) -> None:
+    """Test Retrieval-Augmented Generation with automatic retrieval."""
+    # Answer a question that requires RAG.
+    user_prompt = "How does Einstein define 'simultaneous events' in his special relativity paper?"
+    messages = [{"role": "user", "content": user_prompt}]
+    chunk_spans = []
+    stream = rag(messages, on_retrieval=lambda x: chunk_spans.extend(x), config=raglite_test_config)
+    answer = ""
+    for update in stream:
+        assert isinstance(update, str)
+        answer += update
+    assert "event" in answer.lower()
+    # Verify that RAG context was retrieved automatically.
+    assert [message["role"] for message in messages] == ["user", "assistant", "tool", "assistant"]
+    assert json.loads(messages[-2]["content"])
+    assert chunk_spans
+    assert all(isinstance(chunk_span, ChunkSpan) for chunk_span in chunk_spans)
+
+
+def test_rag_auto_without_retrieval(raglite_test_config: RAGLiteConfig) -> None:
+    """Test Retrieval-Augmented Generation with automatic retrieval."""
+    # Answer a question that does not require RAG.
+    user_prompt = "Is 7 a prime number?"
+    messages = [{"role": "user", "content": user_prompt}]
+    chunk_spans = []
+    stream = rag(messages, on_retrieval=lambda x: chunk_spans.extend(x), config=raglite_test_config)
+    answer = ""
+    for update in stream:
+        assert isinstance(update, str)
+        answer += update
+    # Verify that no RAG context was retrieved.
+    assert [msg["role"] for msg in messages] == ["user", "assistant"]
+    assert not chunk_spans
