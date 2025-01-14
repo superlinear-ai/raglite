aa705bf3567f623123d969f8a76863b9d8fcc9f7
Manolo Santos <manolo.santos@gmail.com>
2025-01-14 17:58:33 +0100

    test: Fix tests

--- tests/test_split_sentences.py
+++ tests/test_split_sentences.py
@@ -1,5 +1,6 @@
 """Test RAGLite's sentence splitting functionality."""
 
+import re
 from pathlib import Path
 
 from raglite._markdown import document_to_markdown
@@ -12,8 +13,8 @@ def test_split_sentences() -> None:
     doc = document_to_markdown(doc_path)
     sentences = split_sentences(doc)
     expected_sentences = [
-        "# ON THE ELECTRODYNAMICS OF MOVING BODIES\n\n",
-        "## By A. EINSTEIN June 30, 1905\n\n",
+        "# ON THE ELECTRODYNAMICS OF MOVING BODIES\n",
+        "## By A. EINSTEIN June 30, 1905\n",
         "It is known that Maxwell’s electrodynamics—as usually understood at the\npresent time—when applied to moving bodies, leads to asymmetries which do\nnot appear to be inherent in the phenomena. ",  # noqa: RUF001
         "Take, for example, the recipro-\ncal electrodynamic action of a magnet and a conductor. ",
         "The observable phe-\nnomenon here depends only on the relative motion of the conductor and the\nmagnet, whereas the customary view draws a sharp distinction between the two\ncases in which either the one or the other of these bodies is in motion. ",
@@ -25,17 +26,17 @@ def test_split_sentences() -> None:
         "We will raise this conjecture (the purport\nof which will hereafter be called the “Principle of Relativity”) to the status\nof a postulate, and also introduce another postulate, which is only apparently\nirreconcilable with the former, namely, that light is always propagated in empty\nspace with a definite velocity c which is independent of the state of motion of the\nemitting body. ",
         "These two postulates suffice for the attainment of a simple and\nconsistent theory of the electrodynamics of moving bodies based on Maxwell’s\ntheory for stationary bodies. ",  # noqa: RUF001
         "The introduction of a “luminiferous ether” will\nprove to be superfluous inasmuch as the view here to be developed will not\nrequire an “absolutely stationary space” provided with special properties, nor\n",
-        "1The preceding memoir by Lorentz was not at this time known to the author.\n\n",
+        "1The preceding memoir by Lorentz was not at this time known to the author.\n",
         "assign a velocity-vector to a point of the empty space in which electromagnetic\nprocesses take place.\n",
         "The theory to be developed is based—like all electrodynamics—on the kine-\nmatics of the rigid body, since the assertions of any such theory have to do\nwith the relationships between rigid bodies (systems of co-ordinates), clocks,\nand electromagnetic processes. ",
-        "Insufficient consideration of this circumstance\nlies at the root of the difficulties which the electrodynamics of moving bodies\nat present encounters.\n\n",
-        "## I. KINEMATICAL PART  § **1. Definition of Simultaneity**\n\n",
+        "Insufficient consideration of this circumstance\nlies at the root of the difficulties which the electrodynamics of moving bodies\nat present encounters.\n",
+        "## I. KINEMATICAL PART § **1. Definition of Simultaneity**\n",
         "Let us take a system of co-ordinates in which the equations of Newtonian\nmechanics hold good.2 ",
     ]
     assert isinstance(sentences, list)
-    assert all(
-        sentence == expected_sentence
-        for sentence, expected_sentence in zip(
-            sentences[: len(expected_sentences)], expected_sentences, strict=True
-        )
-    )
+    # Remove repeated \n to make it more resilient to variations between pdftext versions
+    sentences = [re.sub(r"\n+", "\n", sentence) for sentence in sentences]
+    for sentence, expected_sentence in zip(
+        sentences[: len(expected_sentences)], expected_sentences, strict=True
+    ):
+        assert sentence == expected_sentence
