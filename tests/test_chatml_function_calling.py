8ae3f7d311c5955e280a69e9c0f924735858a120
Laurent Sorber <laurent.sorber@gmail.com>
2025-05-13 09:29:29 +0200

    test: switch to Qwen3 4B for nonaccelerated tests

--- tests/test_chatml_function_calling.py
+++ tests/test_chatml_function_calling.py
@@ -59,7 +59,7 @@ def is_accelerator_available() -> bool:
 @pytest.mark.parametrize(
     "llm_repo_id",
     [
-        pytest.param("bartowski/Llama-3.2-3B-Instruct-GGUF", id="llama_3.2_3B"),
+        pytest.param("unsloth/Qwen3-4B-GGUF", id="qwen3_4B"),
         pytest.param(
             "unsloth/Qwen3-8B-GGUF",
             id="qwen3_8B",
